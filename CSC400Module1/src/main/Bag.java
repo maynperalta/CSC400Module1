729be5601653263f9014307b565d283f9bcdedcb
Maynard Peralta <maynperalta@gmail.com>
2026-01-17 18:54:27 -0700

    Added comments to explain functionality

--- CSC400Module1/src/main/Bag.java
+++ CSC400Module1/src/main/Bag.java
@@ -2,27 +2,27 @@ package main;
 
 import java.util.ArrayList;
 import java.util.List;
-
+// Generic Bag (multiset) implementation that allows duplicate elements
 class Bag<T> {
-	
+// Using List "items" and item type "T"	
 	private List<T> items;
-	
+//	Constructor to initialize Bag
 	public Bag() {
 		items = new ArrayList<>();
 	}
-	
+// Add an item to the Bag	
 	public void add(T item) {
 		items.add(item);
 	}
-	
+// Remove an item from the Bag	
 	public void remove(T item) {
 		items.remove(item);
 	}
-
+// Checks if bag contains item
 	public boolean contains(T item) {
 		return items.contains(item);
 	}
-	
+// Loop through items list for each item to count number of times item appears
 	public int count(T item) {
 		int count = 0;
 		for(T element : items) {
@@ -32,7 +32,7 @@ class Bag<T> {
 		}
 		return count;
 	}
-	
+// Print Bag contents	
 	public void printBag() {
 		System.out.println(items);
 	}
--- CSC400Module1/src/main/Main.java
+++ CSC400Module1/src/main/Main.java
@@ -1,11 +1,10 @@
 package main;
 
 public class Main {
-
 	public static void main(String[] args) {
-		
+// New instance of Bag class		
 		Bag<String> bag = new Bag<>();
-			
+// Add objects to bag (including duplicates)			
 			bag.add("book");
 			bag.add("pen");
 			bag.add("pencil");
@@ -15,27 +14,27 @@ public class Main {
 			bag.add("notebook");
 			bag.add("pen");
 			bag.add("book");
-		
+// Print Bag contents		
 			System.out.printf("\nCurrently in bag: ");
 			bag.printBag();
-			
+// Test "contains" method for a few elements			
 			System.out.printf("\nBag contains book? %b", bag.contains("book"));
 			System.out.printf("\nBag contains pen? %b", bag.contains("pen"));
 			System.out.printf("\nBag contains eraser? %b ",  bag.contains("eraser"));
-		
+// New line for readability, then test "count" method for a few elements		
 			System.out.println("");
 			System.out.printf("\nNumber of books in the bag: %d", bag.count("book"));
 			System.out.printf("\nNumber of pens in the bag: %d", bag.count("pen"));
 			System.out.printf("\nNumber of erasers in the bag: %d", bag.count("eraser"));
 			System.out.println("");
-		
+// Remove elements from Bag		
 			bag.remove("eraser");
 			bag.remove("pen");
 			bag.remove("book");
-		
+// Print Bag contents again		
 			System.out.printf("\nNow in bag: ");
 			bag.printBag();
-		
+// Test "contains" method for removed element and "count" method for removed element		
 			System.out.printf("\nBag contains eraser? %b", bag.contains("eraser"));
 			System.out.printf("\nNumber of pens in bag: %d", bag.count("pen"));
 			System.out.printf("\nNumber of erasers in bag: %d", bag.count("eraser"));
