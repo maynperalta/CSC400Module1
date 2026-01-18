a343a1af8231914b566448d6a03c714655095ec4
Maynard Peralta <maynperalta@gmail.com>
2026-01-17 18:30:02 -0700

    Formatting for readability.

--- CSC400Module1/src/main/Main.java
+++ CSC400Module1/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Bag<String> bag = new Bag<>();
 			
 			bag.add("book");
@@ -15,26 +16,28 @@ public class Main {
 			bag.add("pen");
 			bag.add("book");
 		
-			System.out.println("Currently in bag: ");
+			System.out.printf("\nCurrently in bag: ");
 			bag.printBag();
+			
+			System.out.printf("\nBag contains book? %b", bag.contains("book"));
+			System.out.printf("\nBag contains pen? %b", bag.contains("pen"));
+			System.out.printf("\nBag contains eraser? %b ",  bag.contains("eraser"));
 		
-			System.out.println("Book is in the bag? " + bag.contains("book"));
-			System.out.println("Pen is in the bag? " + bag.contains("pen"));
-			System.out.println("Eraser is in the bag? " + bag.contains("eraser"));
-		
-			System.out.println("Number of books in the bag: " + bag.count("book"));
-			System.out.println("Number of pens in the bag: " + bag.count("pen"));
-			System.out.println("Number of erasers in the bag: " + bag.count("eraser"));
+			System.out.println("");
+			System.out.printf("\nNumber of books in the bag: %d", bag.count("book"));
+			System.out.printf("\nNumber of pens in the bag: %d", bag.count("pen"));
+			System.out.printf("\nNumber of erasers in the bag: %d", bag.count("eraser"));
+			System.out.println("");
 		
 			bag.remove("eraser");
 			bag.remove("pen");
 			bag.remove("book");
 		
-			System.out.println("Now in bag: ");
+			System.out.printf("\nNow in bag: ");
 			bag.printBag();
 		
-			System.out.println("Eraser in bag? " + bag.contains("eraser"));
-			System.out.println("Number of pens in bag: " + bag.count("pen"));
-			System.out.println("Number of erasers in bag: " + bag.count("eraser"));
+			System.out.printf("\nBag contains eraser? %b", bag.contains("eraser"));
+			System.out.printf("\nNumber of pens in bag: %d", bag.count("pen"));
+			System.out.printf("\nNumber of erasers in bag: %d", bag.count("eraser"));
 	}
 }
